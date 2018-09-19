845ee7a2142897a22abc7ef7a40802f4339d9e43
Juan Hernandez <jarceluz@gmail.com>
2018-09-19 09:01:00 -0700

    Ejercicio Numero uno de Programación orientada a objeto

---
+++ Ejercicio1/PrimeraClase/Ejercicio_POO1/Modelos/Persona.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Ejercicio_POO1.Modelos
+{
+    class Persona
+    {
+        public string Nombre;
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        //private int edad;
+
+    }
+}
---
+++ Ejercicio1/PrimeraClase/Ejercicio_POO1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ejercicio_POO1.Modelos;
+namespace Ejercicio_POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Diana";
+            persona.Apellido = "Tacos";
+            persona.Edad = 25;
+            Persona persona2 = new Persona();
+            persona2.Nombre = "Juan";
+            persona2.Apellido = "Kastro";
+            persona2.Edad = 32;
+            NombreCompleto(persona,persona2);
+        }
+
+        private static void NombreCompleto(Persona persona, Persona persona2)
+        {
+            Console.WriteLine(persona.Nombre + " " + persona.Apellido);
+            Console.WriteLine(persona.Edad);
+            Console.WriteLine(persona2.Nombre + " " + persona2.Apellido);
+            Console.WriteLine(persona2.Edad);
+            Console.ReadLine();
+        }
+    }
+}
